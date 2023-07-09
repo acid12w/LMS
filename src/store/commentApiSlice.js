47f2288e6a5ce2350cc48564a60c1c657d322125
Adrian Anderson <adriananderson@Machaelas-MacBook-Air.local>
2023-07-08 21:13:53 -0500

    refactored code for displaying lessons

--- src/store/commentApiSlice.js
+++ src/store/commentApiSlice.js
@@ -3,12 +3,12 @@ import { apiSlice } from "../api/ApiSlice";
 export const commentApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getAllComments: builder.query({
-            query: (id) => `http://localhost:3001/comment/${id}`,
+            query: (id) => `/comment/${id}`,
             providesTags: ["Comment"],
         }),
         addComment: builder.mutation({
             query: (comment) => ({
-                url: `http://localhost:3001/comment`,
+                url: `/comment`,
                 method: 'POST',
                 body: comment 
             }),
@@ -16,7 +16,7 @@ export const commentApiSlice = apiSlice.injectEndpoints({
         }),
         addReply: builder.mutation({
             query: data => ({
-                url: `http://localhost:3001/comment/${data.lessonId}`,
+                url: `/comment/${data.lessonId}`,
                 method: 'PATCH',
                 body: data.reply
             }),
